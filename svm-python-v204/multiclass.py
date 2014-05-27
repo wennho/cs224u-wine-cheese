9e23b58e8b9b266c9d2792a58fe7c1f24d79221e
Wen Hao Lui <whlui@stanford.edu>
2014-05-27 04:16:49 -0700

    fixed multiclass

--- svm-python-v204/multiclass.py
+++ svm-python-v204/multiclass.py
@@ -1,11 +1,11 @@
 """A module for SVM^python for multiclass learning."""
 
-# Thomas Finley, tfinley@gmail.com
-
 import svmapi
+from numpy import *
 
 def read_examples(filename, sparm):
     """Parses an input file into an example sequence."""
+
     # This reads example files of the type read by SVM^multiclass.
     examples = []
     # Open the file and read each example.
@@ -15,15 +15,16 @@ def read_examples(filename, sparm):
         tokens = line.split()
         # If the line is empty, who cares?
         if not tokens: continue
+
         # Get the target.
         target = int(tokens[0])
         # Get the features.
-        tokens = [t.split(':') for t in tokens[1:]]
+        tokens = [tuple(t.split(':')) for t in tokens[1:]]
         features = [(0,1)]+[(int(k),float(v)) for k,v in tokens]
         # Add the example to the list
         examples.append((svmapi.Sparse(features), target))
     # Print out some very useful statistics.
-    print len(examples), 'examples read'
+    print len(examples),'examples read'
     return examples
 
 def init_model(sample, sm, sparm):
@@ -33,19 +34,14 @@ def init_model(sample, sm, sparm):
     # the classifier when reading the model from the file.
     sm.num_features = max(max(x) for x,y in sample)[0]+1
     sm.num_classes = max(y for x,y in sample)
-    sm.size_psi = sm.num_features
-    #print 'size_psi set to',sm.size_psi
+    print sm.num_classes*sm.num_features
+    sm.size_psi = sm.num_features * sm.num_classes
+    print 'size_psi set to',sm.size_psi
 
-thecount = 0
 def classification_score(x,y,sm,sparm):
     """Return an example, label pair discriminant score."""
     # Utilize the svmapi.Model convenience method 'classify'.
-    score = sm.svm_model.classify(psi(x,y,sm,sparm))
-    global thecount
-    thecount += 1
-    if (sum(abs(w) for w in sm.w)):
-        import pdb; pdb.set_trace()
-    return score
+    return sm.svm_model.classify(psi(x,y,sm,sparm))
 
 def classify_example(x, sm, sparm):
     """Returns the classification of an example 'x'."""
@@ -60,22 +56,82 @@ def find_most_violated_constraint(x, y, sm, sparm):
     # Similar, but include the loss.
     scores = [(classification_score(x,c,sm,sparm)+loss(y,c,sparm), c)
               for c in xrange(1,sm.num_classes+1)]
-    ybar = max(scores)[1]
-    #print y, ybar
     return max(scores)[1]
 
 def psi(x, y, sm, sparm):
     """Returns the combined feature vector Psi(x,y)."""
     # Just increment the feature index to the appropriate stack position.
-    #vecness = [(k,v) for k,v in x]
-    # print x
-
-    pvec = svmapi.Sparse(x, kernel_id=y)
-    #print list(sm.w)
-    #print pveca
-    #import pdb; pdb.set_trace()
-    return svmapi.Document([pvec])
+    offset = sm.num_features * (y-1)
+    pvec = svmapi.Sparse([(k+offset,v) for k,v in x], kernel_id=y)
+    return pvec
 
 def loss(y, ybar, sparm):
-    """Loss is 1 if the labels are different, 0 if they are the same."""
-    return 100.0*int(y != ybar)
+    # We use zero-one loss.
+    if y==ybar: return 0
+    return 1
+
+
+def eval_prediction(exnum, (x, y), ypred, sm, sparm, teststats):
+    """Accumulate statistics about a single training example.
+
+    Allows accumulated statistics regarding how well the predicted
+    label ypred for pattern x matches the true label y.  The first
+    time this function is called teststats is None.  This function's
+    return value will be passed along to the next call to
+    eval_prediction.  After all test predictions are made, the last
+    value returned will be passed along to print_testing_stats.
+
+    On the first call, that is, when exnum==0, teststats==None.  The
+    default behavior is that the function does nothing."""
+    if exnum==0: teststats = []
+    teststats.append((y,ypred));
+    return teststats
+
+def print_testing_stats(sample, sm, sparm, teststats):
+    """Print statistics once classification has finished.
+
+    This is called after all test predictions are made to allow the
+    display of any summary statistics that have been accumulated in
+    the teststats object through use of the eval_prediction function.
+
+    The default behavior is that nothing is printed."""
+
+
+    aggConfusionMatrix=zeros((sm.num_classes,sm.num_classes), dtype='i')
+    prec = []
+    recall = []
+    ltc = []
+    lpc=[]
+    for t in teststats:
+        aggConfusionMatrix[t[1]-1,t[0]-1]+=1
+    tc = 0;
+    for i  in xrange(0,sm.num_classes):
+        sum = 0;
+        for j in xrange(0,sm.num_classes):
+            sum+= aggConfusionMatrix[i,j]
+        tc += sum
+        if (sum !=0):
+            prec.append(aggConfusionMatrix[i,i]*100.0/sum);
+        else:
+            prec.append(0);
+        lpc.append(sum);
+    for i  in xrange(0,sm.num_classes):
+        sum = 0;
+        for j in xrange(0,sm.num_classes):
+            sum+= aggConfusionMatrix[j,i]
+        if (sum !=0):
+            recall.append(aggConfusionMatrix[i,i]*100.0/sum);
+        else:
+            recall.append(0);
+        ltc.append(sum);
+    for i in xrange(0,sm.num_classes):
+        print "label ", i+1 , " prec: ", prec[i], " recall: ", recall[i], " tp: ", aggConfusionMatrix[i,i], " tc: ",ltc[i], " pc: ",lpc[i]
+    match = 0
+    for i in xrange(0,sm.num_classes):
+        match += aggConfusionMatrix[i,i]
+
+    print "prec: ", match*100.0/tc , "recall: ",match*100.0/tc, "tp: ",match, " pc: ", tc, "tc: ",tc
+
+    print "confusion matrix:"
+    print aggConfusionMatrix;
+    savetxt('conf.txt',aggConfusionMatrix);
